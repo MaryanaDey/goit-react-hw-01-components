b50681f51a246506e3414c8603f3e14d7f0b318a
MaryanaDey <81640002+MaryanaDey@users.noreply.github.com>
2021-12-28 18:33:47 +0200

    add revisions

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,32 +1,32 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import s from './Statistics.module.css'
+import React from "react";
+import PropTypes from "prop-types";
+import s from "./Statistics.module.css";
 
-import Statistics from './StatisticsList';
+import Statistics from "./StatisticsList";
 
-export default function StatisticsList({ stats }) {
-    return (
-        <section className={ s.section }>
-        <h2 className={s.title}>Upload stats</h2>
-      
-        <ul className={s.statistics}>
-          {stats.map(item => (
-             <Statistics
-             key = {item.id}
-             label={item.label}
-             percentage={item.percentage}
-              /> 
-          ))}
-        </ul>
-      </section>
-    );
-};
+export default function StatisticsList({ stats, title }) {
+  return (
+    <section className={s.section}>
+      {title && <h2 className={s.title}>Upload stats</h2>}
 
+      <ul className={s.statistics}>
+        {stats.map((item) => (
+          <Statistics
+            key={item.id}
+            label={item.label}
+            percentage={item.percentage}
+          />
+        ))}
+      </ul>
+    </section>
+  );
+}
 
 Statistics.propTypes = {
-items: PropTypes.arrayOf(
+  items: PropTypes.arrayOf(
     PropTypes.shape({
-        id: PropTypes.string,
+      id: PropTypes.string,
+      title: PropTypes.string,
     })
-)
-}
+  ),
+};
--- src/components/Statistics/StatisticsList.js
+++ src/components/Statistics/StatisticsList.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import s from './Statistics.module.css';
+import React from "react";
+import PropTypes from "prop-types";
+import s from "./Statistics.module.css";
 
-export default function StaticsList({label, percentage}) {
-    return(
-        <li className={s.item}>
-      <span >{label}</span>
-      <span >{percentage}</span>
+export default function StaticsList({ label, percentage }) {
+  return (
+    <li className={s.item}>
+      <span>{label}</span>
+      <span>{percentage}%</span>
     </li>
-    )
+  );
 }
 
 StaticsList.propTypes = {
-    label:PropTypes.string,
-    percentage: PropTypes.number,
-}
+  label: PropTypes.string,
+  percentage: PropTypes.number,
+};
